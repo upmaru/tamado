71ae5d90e0c5a566345a4f423b0b1bd431a2dc96
Zack Siri <me@zacksiri.dev>
2026-08-24 19:24:17 +0700

    add lucide icons

--- config/initializers/rails_icons.rb
+++ config/initializers/rails_icons.rb
@@ -1,3 +1,11 @@
 RailsIcons.configure do |config|
   config.default_library = "lucide"
+
+  # Override Lucide defaults
+  # config.libraries.lucide.default_variant = "" # Set a default variant for Lucide
+  # config.libraries.lucide.exclude_variants = [] # Exclude specific variants
+
+  # config.libraries.lucide.outline.default.css = "size-6"
+  # config.libraries.lucide.outline.default.stroke_width = "1.5"
+  # config.libraries.lucide.outline.default.data = {}
 end
