d1e0093441013d3b2f0366bd0a3c78cc68b83848
Zack Siri <me@zacksiri.dev>
2026-08-24 19:31:53 +0700

    Fix border radius

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount RailsIcons::Engine, at: '/rails_icons'
+  if Rails.env.development?
+    mount RailsIcons::Engine, at: "/rails_icons"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
